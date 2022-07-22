fa565694700e0039f1f18219a36f760d51ba69be
Guillaume <g.jaskula@me.com>
2022-07-23 02:57:14 +0300

    added basic operations in javascript

--- Tutorial/renderer.js
+++ Tutorial/renderer.js
@@ -1,6 +1,13 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
+const nameinput = document.getElementById('nameinput')
+const firstbutton = document.getElementById('firstbutton')
+const texttochange = document.getElementById('texttochange')
+
+firstbutton.onclick = function() {
+    texttochange.innerText = "Well hello there, " + nameinput.value;
+};
 
 
 const func = async () => {
